e6a1313d53ea891df16e1fa07b97a4ca0196756a
Robert Cunningham <rptc3000@gmail.com>
2015-04-01 10:55:41 -0400

    migrations, models, server.js, backbone folders and jesus

---
+++ models/artworks.js
@@ -0,0 +1,26 @@
+"use strict"
+module.exports = function(sequelize, DataTypes) {
+  var artworks = sequelize.define('artworks', {
+    artist: DataTypes.STRING,
+    title: DataTypes.STRING,
+    date: DataTypes.DATEONLY,
+    medium: DataTypes.STRING,
+    img_url: DataTypes.TEXT,
+    start_date: DataTypes.INTEGER,
+    end_date: DataTypes.INTEGER,
+    culture: DataTypes.STRING
+  }, {
+    timestamps: false,
+
+    classMethods: {
+      associate: function(models) {
+        artworks.belongsToMany(models.users, {
+          through: 'artworks_users',
+          foreignKey: 'artwork_id'
+        });
+        artworks.hasMany(models.comments, { foreignKey: 'artwork_id' });
+      }
+    }
+  });
+  return artworks;
+};
---
+++ models/comments.js
@@ -0,0 +1,21 @@
+"use strict"
+modules.exports = function(sequelize, DataTypes) {
+  var comments = sequelize.define('comments', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    date: { DataTypes.DATE, defaultValue: Sequelize.NOW },
+    artwork_id: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER
+  }, {
+    timestamps: false,
+
+    classMethods: {
+      associate: function(models) {
+        comments.belongsTo(models.users, { foreignKey: 'user_id' });
+
+        comments.belongsTo(models.artworks, { foreignKey: 'artwork_id' });
+      }
+    }
+  });
+  return comments;
+};
