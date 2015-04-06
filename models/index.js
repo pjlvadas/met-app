9872933b6c9aca817fea1decf61bad399cd4f14d
Ryan Matuszewski <rmmatuszewski@gmail.com>
2015-04-06 12:41:59 -0400

    heroku database config

--- models/index.js
+++ models/index.js
@@ -7,6 +7,16 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (process.env.HEROKU_POSTGRESQL_[ROSE]_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_[ROSE]_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      logging:  true //false
+    })
+  } else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 var db        = {};
 
 fs
