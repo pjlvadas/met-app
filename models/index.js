9993f67896f2b0e22bbe5b6cf9fd7aa41b14df55
Ryan Matuszewski <rmmatuszewski@gmail.com>
2015-04-06 12:57:25 -0400

    typo in models/index.js

--- models/index.js
+++ models/index.js
@@ -7,9 +7,10 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
-if (process.env.HEROKU_POSTGRESQL_[ROSE]_URL) {
+
+if (process.env.HEROKU_POSTGRESQL_ROSE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_[ROSE]_URL, {
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_ROSE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
       logging:  true //false
@@ -17,6 +18,7 @@ if (process.env.HEROKU_POSTGRESQL_[ROSE]_URL) {
   } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
   }
+
 var db        = {};
 
 fs
